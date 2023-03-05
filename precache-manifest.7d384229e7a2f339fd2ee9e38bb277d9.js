3e0113985ec622288ae7cb9b9de6ac00a17ef95a
Aryan <github@aryanc403.com>
2023-03-05 22:03:32 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.7d384229e7a2f339fd2ee9e38bb277d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1c223fa262b23935cbf",
+    "url": "/um-checklist/static/js/main.8cb38451.chunk.js"
+  },
+  {
+    "revision": "282fcd9d5e260fc85da6",
+    "url": "/um-checklist/static/js/runtime~main.28e31fd8.js"
+  },
+  {
+    "revision": "5c071678c5bee2fb0fd5",
+    "url": "/um-checklist/static/js/2.ff1a3071.chunk.js"
+  },
+  {
+    "revision": "204debee611230c0976d27ccb4ff2b07",
+    "url": "/um-checklist/index.html"
+  }
+];
